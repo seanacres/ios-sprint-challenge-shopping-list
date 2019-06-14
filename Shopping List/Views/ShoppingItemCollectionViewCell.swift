1465bb236c593d479f7b0936a7144c74cf0255cf
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-14 11:25:17 -0400

    Set up files and outlets in files

---
+++ Shopping List/Views/ShoppingItemCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingItemCollectionViewCell.swift
+//  Shopping List
+//
+//  Created by Sean Acres on 6/14/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingItemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var hasBeenAddedLabel: UILabel!
+    @IBOutlet weak var shoppingItemImageView: UIImageView!
+    @IBOutlet weak var shoppingItemNameLabel: UILabel!
+    
+}
